7005162441df0cb139fcd72cbb7020ae0d6dfce8
Konrad Gontarczyk <konrad326@opoczta.pl>
2024-05-05 23:34:35 +0200

    New connection with lesson

--- src/main/java/pl/gontarczyk/tutoringmanagement/language/model/Language.java
+++ src/main/java/pl/gontarczyk/tutoringmanagement/language/model/Language.java
@@ -2,6 +2,7 @@ package pl.gontarczyk.tutoringmanagement.language.model;
 
 import jakarta.persistence.*;
 import lombok.*;
+import pl.gontarczyk.tutoringmanagement.lesson.model.Lesson;
 import pl.gontarczyk.tutoringmanagement.student.model.Student;
 import pl.gontarczyk.tutoringmanagement.teacher.model.Teacher;
 
@@ -28,6 +29,9 @@ public class Language {
     )
     private Set<Teacher> teachers;
 
-    @OneToMany(mappedBy = "language")
+    @OneToMany(mappedBy = "language", fetch = FetchType.LAZY)
     private Set<Student> student;
+
+    @OneToMany(mappedBy = "language", fetch = FetchType.LAZY)
+    private Set<Lesson> lessons;
 }
